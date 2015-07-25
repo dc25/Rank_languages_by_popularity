e232798a581942108c47e9e82815bd8a290b7026
Dave Compton <davecompton7@gmail.com>
2015-07-25 05:59:30 -0700

    reformatted; changed some names

--- Main.hs
+++ Main.hs
@@ -8,46 +8,54 @@ import Data.Function (on)
 
 import qualified Data.Map as M
 
-data Page =
-    Page { 
-             title     :: String,
-             size      :: Int
-         } deriving (Show)
+data Language =
+    Language { 
+        name      :: String,
+        quantity  :: Int
+    } deriving (Show)
 
-instance FromJSON Page where
+instance FromJSON Language where
     parseJSON (Object p) = do
         categoryInfo <- p .:? "categoryinfo" 
-        let size = case categoryInfo of
-                       Just ob -> ob .: "size"
-                       Nothing -> return 0
-            title = p .: "title"
-        Page <$> title <*> size
 
-data Languages =
-    Languages { 
-                gcmcontinue    :: Maybe String,
-                pages          :: M.Map String Page
-              } deriving (Show)
+        let quantity = case categoryInfo of
+                           Just ob -> ob .: "size"
+                           Nothing -> return 0
 
-instance FromJSON Languages where
+            name = p .: "title"
+
+        Language <$> name <*> quantity
+
+data Report =
+    Report { 
+        gcmcontinue    :: Maybe String,
+        languages      :: M.Map String Language
+    } deriving (Show)
+
+instance FromJSON Report where
     parseJSON (Object p) = do
         querycontinue <- p .:? "query-continue"
+
         let gcmcontinue 
                 = case querycontinue of
-                      Just ob -> fmap Just $ (ob .: "categorymembers") >>=
-                                                (.: "gcmcontinue")
+                      Just ob -> fmap Just $ 
+                                     (ob .: "categorymembers") >>= 
+                                     (   .: "gcmcontinue")
                       Nothing -> return Nothing
 
-            pages = (p .: "query") >>= (.: "pages") 
-        Languages <$> gcmcontinue <*> pages
+            languages = (p .: "query") >>= (.: "pages") 
+
+        Report <$> gcmcontinue <*> languages
 
 baseQuery = "http://rosettacode.org/mw/api.php?format=json&action=query&generator=categorymembers&gcmtitle=Category:Programming%20Languages&gcmlimit=500&prop=categoryinfo" 
 
-runQuery :: String -> IO [Page]
+runQuery :: String -> IO [Language]
 runQuery query = do
-    Just decoded <- decode <$> simpleHttp query 
-    let res = map snd $ M.toList $ pages decoded 
-    moreRes <- case gcmcontinue decoded of
+    Just report <- decode <$> simpleHttp query 
+
+    let res = map snd $ M.toList $ languages report 
+
+    moreRes <- case gcmcontinue report of
                       Nothing -> return []
                       Just cs -> runQuery $ 
                                      baseQuery 
@@ -58,5 +66,13 @@ runQuery query = do
 main :: IO ()
 main = do 
     allLanguages <- runQuery baseQuery
-    mapM_ showPage $ zip [1..] $ sortBy (flip compare `on` size) allLanguages
-        where showPage pg = putStrLn $ show (fst pg) ++ " " ++ drop 9 (title $ snd pg) ++ " " ++ show  (size $ snd pg)
+
+    mapM_ showPage $ 
+          zip [1..] $ 
+          sortBy (flip compare `on` quantity) allLanguages
+
+        where showPage pg 
+                  = putStrLn $ 
+                        show (fst pg) ++ 
+                        " " ++ drop 9 (name $ snd pg) ++ 
+                        " " ++ show  (quantity $ snd pg)
