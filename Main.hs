f33a57c203c02ec2c416601621182be90b83c065
Dave Compton <davecompton7@gmail.com>
2015-07-26 19:35:43 +0000

    continued code cleanup

--- Main.hs
+++ Main.hs
@@ -51,6 +51,29 @@ instance FromJSON Report where
 
         Report <$> continue <*> languages
 
+-- Pretty print a single language
+showLanguage :: Int -> Bool -> Language -> IO ()
+showLanguage rank tie (Language languageName languageQuantity) = 
+    let rankStr = show rank
+    in putStrLn $ rankStr ++ "." ++ 
+                      replicate (4 - length rankStr) ' ' ++
+                      (if tie then " (tie)" else "      ") ++
+                      " " ++ drop 9 languageName ++
+                      " - " ++ show languageQuantity
+
+-- Pretty print languages with common rank
+showRanking :: (Int,  [Language]) -> IO ()
+showRanking (ranking, languages) = 
+    mapM_ (showLanguage ranking $ length languages > 1) languages
+
+-- Sort and group languages by rank, then pretty print them.
+showLanguages :: [Language] -> IO ()
+showLanguages allLanguages =
+    mapM_ showRanking $ 
+          zip [1..] $ 
+          groupBy ((==) `on` quantity) $
+          sortBy (flip compare `on` quantity) allLanguages
+
 -- Mediawiki api style query to send to rosettacode.org
 queryStr = "http://rosettacode.org/mw/api.php?" ++ 
            "format=json" ++ 
@@ -61,44 +84,17 @@ queryStr = "http://rosettacode.org/mw/api.php?" ++
            "&prop=categoryinfo" 
 
 -- Issue query to get a list of Language descriptions
-runQuery :: String -> IO [Language]
-runQuery query = do
-    Just report <- decode <$> simpleHttp query 
-
-    let res = map snd $ toList $ languages report 
+runQuery :: [Language] -> String -> IO ()
+runQuery ls query = do
+    Just (Report continue langs) <- decode <$> simpleHttp query 
+    let accLanguages = ls ++ map snd (toList langs)
 
     -- If there is a continue string, recusively continue the query.
-    moreRes <- case continue report of
-                      Nothing -> return []
-                      Just continueStr -> runQuery $ 
-                                              queryStr ++ 
-                                              "&gcmcontinue=" ++ 
-                                              urlEncode continueStr
-    return $ res ++ moreRes
-
--- Pretty print languages and ranks 
-showRanking :: (Int,  [Language]) -> IO ()
-showRanking languageGroup = do
-    let ranking = fst languageGroup
-    let languages = snd languageGroup
-    let tie = length languages > 1
-
-    mapM_ (showLanguage ranking tie) languages
-
-    where showLanguage r t lang = 
-              let rankStr = show r
-              in putStrLn $ rankStr ++ "." ++ 
-                            replicate (4 - length rankStr) ' ' ++
-                            (if t then " (tie)" else "      ") ++
-                            " " ++ drop 9 (name lang) ++
-                            " - " ++ show  (quantity lang) 
+    case continue of
+        Nothing -> showLanguages accLanguages
+        Just continueStr -> do
+            let continueQueryStr = queryStr ++ "&gcmcontinue=" ++ urlEncode continueStr
+            runQuery accLanguages continueQueryStr
 
 main :: IO ()
-main = do 
-    allLanguages <- runQuery queryStr
-
-    mapM_ showRanking $ 
-          zip [1..] $ 
-          groupBy ((==) `on` quantity) $
-          sortBy (flip compare `on` quantity) allLanguages
-
+main = runQuery [] queryStr
