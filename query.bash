c7aec6f5a31ecdf368cb8571de3302968637efbc
Dave Compton <davecompton7@gmail.com>
2015-08-13 17:46:20 -0700

    adding an example query.

---
+++ query.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -O - "http://rosettacode.org/mw/api.php?format=json&action=query&generator=categorymembers&gcmtitle=Category:Programming%20Languages&gcmlimit=500&prop=categoryinfo"  
